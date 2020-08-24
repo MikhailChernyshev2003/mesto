01fecf4fada561d46c13ab28aa74673cc2dec213
Mikhail Chernyshev <M.Chernyshev2003@yandex.ru>
2020-08-25 01:06:24 +0800

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,7 @@ const addCardPopup = document.querySelector('.addPopup');
 const imgPopup = document.querySelector('.imgPopup');
 const openButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const closeButton = popup.querySelector('.popup__close-button');
-const secondCloseButton = addCardPopup.querySelector('.popup__close-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 const imgCloseButton = document.querySelector('.popup__close-img-button');
 const saveButton = popup.querySelector('.popup__container-button');
 const secondSaveButton = addCardPopup.querySelector('.popup__container-button');
@@ -12,7 +11,7 @@ const inputName = popup.querySelector('#name');
 const inputJob = popup.querySelector('#job');
 const inputTitle = addCardPopup.querySelector('#title');
 const inputLink = addCardPopup.querySelector('#link');
-const name = document.querySelector('.profile__name');
+const name = document.querySelector('.profile__name span');
 const job = document.querySelector('.profile__status');
 const form = document.querySelector('.popup__container');
 const img = document.querySelectorAll('.element__image');
@@ -44,106 +43,91 @@ const initialCards = [
     }
 ];
 
-function togglePopup(evt) {
-    if(evt.target === openButton || evt.target === closeButton){
-    /*switch (item){
-        case 'popup':
-            popup.classList.toggle('popup_opened');
-            if(popup.classList.contains('popup_opened')){
-            inputName.value=name.textContent;
-            inputJob.value=job.textContent;
-            }
-            break;
-        case 'addCardPopup':
-            addCardPopup.classList.toggle('popup_opened');
-            break;
-        case 'imgPopup':
-            imgPopup.classList.toggle('popup_opened');    
-    }*/
-    popup.classList.toggle('popup_opened');
+function handleTogglePopup(evt) {
+    const popupForToggle = document.querySelector(evt.target.dataset.popup);
+    if (popupForToggle) {
+        togglePopup(popupForToggle);
     }
-    else{
-        //addCardPopup.classList.toggle('popup_opened')
-    }
-    
-    inputTitle.value = '';
-    inputLink.value = '';   
 }
-/*const addPopupToggle = function (evt) {
-    addCardPopup.classList.toggle('popup_opened');
-    inputTitle.value = '';
-    inputLink.value = ''; 
-}*/
 
-function editFormSubmitHandler (evt) {
-    evt.preventDefault(); 
+function togglePopup(popupForToggle) {
+    popupForToggle.classList.toggle('popup_opened');
+}
+
+function editFormSubmitHandler(evt) {
+    evt.preventDefault();
     job.textContent = inputJob.value;
-    //name.textContent = inputName.value;
-    name.innerHTML = inputName.value + '<button class="profile__edit-button" type="button"></button>';
-    popup.classList.toggle('popup_opened');
+    name.textContent = inputName.value;
+    togglePopup(popup);
 }
 
-function addElement (evt){
-    evt.preventDefault(); 
+function createElement() {
     const elementTemplate = document.querySelector('#element').content;
-    const elements = document.querySelector('.elements');
     const element = elementTemplate.cloneNode(true);
+    const image = element.querySelector('.element__image');
     element.querySelector('.element__paragraph').textContent = inputTitle.value;
-    element.querySelector('.element__image').src = inputLink.value;
-    const cardTitle = inputTitle.value; // buff - сокращение от buffer, так как использовал переменную для временного хранения данных) 
-    const cardLink = inputLink.value;
-    element.querySelector('.element__heart-button').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__heart-button_active');
-    });
-    element.querySelector('.element__image').addEventListener('click', function (evt) {
-        document.querySelector('.popup__text').textContent = cardTitle;
-        document.querySelector('.popup__image').src = cardLink;
-        imgPopup.classList.toggle('popup_opened');
-    });
-    elements.prepend(element);
-    const deleteButton = document.querySelectorAll('.element__delete-button');
-    deleteButton.forEach(function (item) {
-        item.addEventListener('click', (evt) => {
-            const targetElement = evt.target;
-            targetElement.parentNode.remove();
+    image.src = inputLink.value;
+    element.querySelector('.element__heart-button')
+        .addEventListener('click', function (evt) {
+            evt.target.classList.toggle('element__heart-button_active');
         });
+    image.addEventListener('click', function (evt) {
+        document.querySelector('.popup__text').textContent = inputTitle.value;
+        document.querySelector('.popup__image').src = inputLink.value;
+        togglePopup(imgPopup);
     });
-    addCardPopup.classList.toggle('popup_opened');
-    inputTitle.value = '';
-    inputLink.value = '';
+    return element;
 }
 
-initialCards.forEach(function (initialCards){
-    const elementTemplate = document.querySelector('#element').content;
-    const elements = document.querySelector('.elements');
-    const element = elementTemplate.cloneNode(true);
-    element.querySelector('.element__paragraph').textContent = initialCards.name;
-    element.querySelector('.element__image').src = initialCards.link;
-    element.querySelector('.element__heart-button').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__heart-button_active');
-    });
-    element.querySelector('.element__image').addEventListener('click', function (evt) {
-        document.querySelector('.popup__text').textContent = initialCards.name;
-        document.querySelector('.popup__image').src = initialCards.link;
-        imgPopup.classList.toggle('popup_opened');
-    });
-    elements.prepend(element);
-});
-
-const deleteButton = document.querySelectorAll('.element__delete-button');
-deleteButton.forEach(function (item) {
+const setEventListenerForDeleteButton = function (item) {
     item.addEventListener('click', (evt) => {
         const targetElement = evt.target;
         targetElement.parentNode.remove();
     });
-});
+}
 
-addButton.addEventListener('click', togglePopup('addCardPopup'));
-saveButton.addEventListener('click', editFormSubmitHandler); 
-openButton.addEventListener('click', togglePopup('popup'));
-closeButton.addEventListener('click', togglePopup('popup'));
-secondCloseButton.addEventListener('click', togglePopup)('addCardPopup');
+function addElement(evt) {
+    evt.preventDefault();
+    const elements = document.querySelector('.elements');
+    const newElement = createElement();
+    elements.prepend(newElement);
+    const deleteButton = document.querySelectorAll('.element__delete-button');
+    deleteButton.forEach(setEventListenerForDeleteButton);
+    togglePopup(addCardPopup);
+    inputTitle.value = '';
+    inputLink.value = '';
+}
+
+function initializationCards (initialCards) {
+    const elementTemplate = document.querySelector('#element').content;
+    const elements = document.querySelector('.elements');
+    const element = elementTemplate.cloneNode(true);
+    const image = element.querySelector('.element__image');
+    element.querySelector('.element__paragraph').textContent = initialCards.name;
+    image.src = initialCards.link;
+    element.querySelector('.element__heart-button').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('element__heart-button_active');
+    });
+    image.addEventListener('click', function (evt) {
+        document.querySelector('.popup__text').textContent = initialCards.name;
+        document.querySelector('.popup__image').src = initialCards.link;
+        togglePopup(imgPopup);
+    });
+    elements.prepend(element);
+}
+initialCards.forEach(initializationCards);
+
+const setEventListenerForCloseButton = function (item) {
+    item.addEventListener('click', handleTogglePopup);
+}
+closeButtons.forEach(setEventListenerForCloseButton);
+
+const deleteButton = document.querySelectorAll('.element__delete-button');
+deleteButton.forEach(setEventListenerForDeleteButton);
+openButton.addEventListener('click', handleTogglePopup);
+addButton.addEventListener('click', handleTogglePopup);
+saveButton.addEventListener('click', editFormSubmitHandler);
 secondSaveButton.addEventListener('click', addElement);
-imgCloseButton.addEventListener('click', function (evt){
-    imgPopup.classList.toggle('popup_opened');
+imgCloseButton.addEventListener('click', function (evt) {
+    togglePopup(imgPopup);
 });
