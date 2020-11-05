79837143c6074ed2d53c6fba3d50c4ee164c82ad
Mikhail Chernyshev <M.Chernyshev2003@yandex.ru>
2020-11-05 22:42:36 +0800

    project 8, attempt 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import '../pages/index.css';
 
-import FormValidator from '../scripts/FormValidator.js';
-import Card from '../scripts/Card.js';
-import PopupWithForm from '../scripts/PopupWithForm.js';
-import PopupWithImage from '../scripts/PopupWithImage.js';
-import Section from '../scripts/Section.js';
-import UserInfo from '../scripts/UserInfo.js'; 
+import FormValidator from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js'; 
 
 import {
     profilePopup,
@@ -25,7 +25,7 @@ import {
     addCardPopupSelector,
     userName,
     userJob
-} from './constants.js';
+} from '../utils/constants.js';
 
 const popupImg = new PopupWithImage(imgPopupSelector);
 popupImg.setImgEventListeners();
